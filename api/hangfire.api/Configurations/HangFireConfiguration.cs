eab5006ed4369e6021b3cef3290985cd8e78aa2d
phillrog <phillrog@hotmail.com>
2022-11-06 10:22:36 -0300

    Configurando projeto

---
+++ api/hangfire.api/Configurations/HangFireConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+
+namespace hangfire.api.Configurations
+{
+    public static class HangFireConfiguration
+    {
+        public static void AddHangFireConfiguration(this IServiceCollection services, IConfiguration app)
+        {
+            services.AddHangfire(configuration => configuration
+                                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                                    .UseSimpleAssemblyNameTypeSerializer()
+                                    .UseRecommendedSerializerSettings()
+                                    .UseSqlServerStorage(
+                                        app.GetConnectionString("agendamento_db"), new SqlServerStorageOptions
+                                        {
+                                            TransactionTimeout = TimeSpan.FromMinutes(60),
+                                            CommandTimeout = TimeSpan.FromMinutes(60),
+                                            CommandBatchMaxTimeout = TimeSpan.FromMinutes(60),
+                                            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(60),
+                                            QueuePollInterval = TimeSpan.Zero,
+                                            UseRecommendedIsolationLevel = true,
+                                            DisableGlobalLocks = true
+                                        }
+                                    )
+                                    .UseSerilogLogProvider()
+                                );
+            services.AddHangfireServer();
+        }
+    }
+}
--- api/hangfire.api/Program.cs
+++ api/hangfire.api/Program.cs
@@ -1,4 +1,13 @@
+using hangfire.api.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                optional: true).AddEnvironmentVariables();
+
+
+builder.Services.AddHangFireConfiguration(builder.Configuration);
 
 // Add services to the container.
 
