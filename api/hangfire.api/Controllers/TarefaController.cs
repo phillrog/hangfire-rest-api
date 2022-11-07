ba035570204f03014c8af6dc48fd74f803c61cf8
phillrog <phillrog@hotmail.com>
2022-11-07 14:36:30 -0300

    Criando um agendamento imediato

---
+++ api/hangfire.api/Controllers/TarefaController.cs
@@ -0,0 +1,25 @@
+﻿using hangfire.api.Models;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hangfire.api.Controllers
+{
+    public class TarefaController : ControllerBase
+    {
+        [HttpPost("Agendar")]
+        public Task<IActionResult> Agendar([FromBody] Agendamento agendamento)
+        {
+            var dataAgendamento = agendamento.DataAgendamento;
+
+            agendamento.DataInicio = DateTime.Now;
+
+            var minutes = (dataAgendamento - DateTime.Now).TotalMinutes;
+
+            var dateTimeOffset = DateTimeOffset.Now.AddMinutes(minutes);
+
+            //var jobId = BackgroundJob.Schedule(() => REQUEST, dateTimeOffset);
+
+            return Ok();
+        }
+    }
+}
---
+++ api/hangfire.api/Models/Agendamento.cs
@@ -0,0 +1,14 @@
+﻿namespace hangfire.api.Models
+{
+    public class Agendamento
+    {
+        public Guid UId => Guid.NewGuid();
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Url { get; set; }
+        public string? Body { get; set; }
+        public DateTime DataAgendamento { get; set; }
+        public DateTime DataInicio { get; set; }
+    }
+}
