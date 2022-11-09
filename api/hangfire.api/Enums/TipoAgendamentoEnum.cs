23b3179264131d016f073b45329d87763b11f6e8
phillrog <phillrog@hotmail.com>
2022-11-08 21:09:11 -0300

    Criação do enum tipos de agendamento

---
+++ api/hangfire.api/Enums/TipoAgendamentoEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace hangfire.api.Enums
+{
+    public enum TipoAgendamentoEnum
+    {
+        [Description("Único e Imediato")]
+        UnicoImediato = 1,
+
+        [Description("Único")]
+        Unico = 2,
+
+        [Description("Diário")]
+        Diario = 3,
+
+        [Description("Semanal")]
+        Semanal = 4,
+
+        [Description("Mensal")]
+        Mensal = 5,
+
+        [Description("Horário")]
+        Horario = 6
+    }
+}
