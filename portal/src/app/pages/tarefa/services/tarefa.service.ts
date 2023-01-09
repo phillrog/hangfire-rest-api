8cac8e06004f3240deabc7148ad67b3d0fde605c
phillrog <phillrog@hotmail.com>
2023-01-09 10:41:29 -0300

    Implementação do request agendar

--- portal/src/app/pages/tarefa/services/tarefa.service.ts
+++ portal/src/app/pages/tarefa/services/tarefa.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { TarefaModel } from '../models/tarefa.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefaService {
+  baseUrl: any;
 
-  constructor() { }
+  constructor( private httpCLient: HttpClient) {
+      this.baseUrl = environment.hangfireTarefaApi;
+  }
+
+  toModel(agendamento: any) : TarefaModel {
+      return new TarefaModel(
+        agendamento.nome,
+        agendamento.url,
+        agendamento.dataAgendamento,
+        agendamento.horaAgendamento,
+        Number(agendamento.tipoAgendamento),
+        Number(agendamento.tipoServico),
+        Number(agendamento.tipoVerbo),
+        agendamento.payload
+      );
+  }
+
+  agendar(agendamento: TarefaModel) : Observable<any> {
+      return this.httpCLient.post(`${this.baseUrl}agendar`, agendamento );
+  }
 }
