ac749231e7e919f2a744a3962ac15db981b1b224
phillrog <phillrog@hotmail.com>
2023-01-12 09:21:41 -0300

    bind e validação dos campos com reactive form

--- portal/src/app/pages/tarefa/incluir/incluir.component.ts
+++ portal/src/app/pages/tarefa/incluir/incluir.component.ts
@@ -1,22 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { TarefaService } from '../services/tarefa.service';
 
 @Component({
   selector: 'ngx-incluir-tarefa',
   styleUrls: ['./incluir.component.scss'],
   templateUrl: './incluir.component.html',
 })
-export class IncluirTarefaComponent {
+export class IncluirTarefaComponent implements OnInit {
 
-  profileForm = this.fb.group({
-    nome: ['', Validators.required]
-  });
+  tarefaForm : FormGroup;
+  submitted = false;
   
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private tarefaService: TarefaService) {
     
+  }
+
+  ngOnInit(): void {
+      this.criarForm();
   }
   
   onSubmit() {
+      this.submitted = true;
+      if(this.tarefaForm.invalid)
+          return;
 
+      const param = this.tarefaService.toModel(this.tarefaForm.value);
+      this.tarefaService.agendar(param).subscribe((response: any) => {
+          this.resetarForm();
+      })
   }
+
+  criarForm() {
+      this.tarefaForm = this.fb.group({
+        nome: ['teste', Validators.required],
+        url: ['https://www.google.com/', Validators.required],
+        tipoAgendamento: ['1', Validators.required],
+        dataAgendamento: ['', Validators.required],
+        horaAgendamento: ['', Validators.required],
+        tipoServico: ['1', Validators.required],
+        tipoVerbo: ['1', Validators.required],
+        payload: [''],
+      });
+    
+  }
+
+  resetarForm() {
+      this.submitted = false;
+      this.tarefaForm.reset();
+  }
+
+  get f() { return this.tarefaForm.controls; }
 }
