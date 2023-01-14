846d5a8bda8ded4e6980661207151a0751d99ccd
phillrog <phillrog@hotmail.com>
2023-01-14 15:24:22 -0300

    Criação model cron para diario mensal

---
+++ api/hangfire.api/Models/Cron.cs
@@ -0,0 +1,15 @@
+﻿namespace hangfire.api.Models
+{
+    public interface ITipoCron { };
+    public class Diario : ITipoCron
+    {
+        public Diario(int hora, int minuto)
+        {
+            Hora = hora;
+            Minuto = minuto;
+        }
+
+        public int Hora { get; set; }
+        public int Minuto { get; set; }
+    }
+}
